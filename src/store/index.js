6585ccb550bd98da5cdb5cb070d8862d6968d18c
BillIvanKoo <kooslarto.bill.ivan@gmail.com>
2020-02-27 22:53:33 +0700

    Modified store only to include redux-logger during development

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { logger } from 'redux-logger';
+import logger from "redux-logger";
 
 import reducer from "../reducers";
 import rootSaga from "../sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
+let middleware = [sagaMiddleware]
+if (process.env.NODE_ENV === 'development') {
+    middleware = [...middleware, logger]
+}
+
 const store = createStore(
     reducer,
-    applyMiddleware(sagaMiddleware, logger),
+    applyMiddleware(...middleware),
 )
 
 sagaMiddleware.run(rootSaga);
